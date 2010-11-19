5042193bcdc02c6acc54cfa9abaf756e1c94d18b
Kishore Kumar Yekkanti <kishoreyekkanti@yahoo.com>
2010-11-20 02:28:09 +0530

    Introduced default location providers and decoupled the location logic from the acitivity.

--- TimeTravel/src/com/travel/activities/UploadDetailsActivity.java
+++ TimeTravel/src/com/travel/activities/UploadDetailsActivity.java
@@ -16,7 +16,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,12 +25,14 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.travel.services.TwitpicUpload;
+import com.travel.utils.CurrentLocation;
 
 public class UploadDetailsActivity extends Activity{
 	private String imagePath;
 	private EditText description;
 	TwitpicUpload twitPicUpload;
 	LocationManager locationManager;
+	Location lastBestKnownLocation;
 	public void onCreate(Bundle savedInstanceState){
 		Log.d("UploadDetailsActivity","Stareted uploaded details activiy");
 		super.onCreate(savedInstanceState);
@@ -42,7 +43,7 @@ public class UploadDetailsActivity extends Activity{
 		Button upload = (Button)findViewById(R.id.photo_upload);
 		
         locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 1000, new MyLocationListener());
+        setLocationProviders();
 		upload.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				Log.d("Upload Details Activity", "triggering the upload");
@@ -57,12 +58,17 @@ public class UploadDetailsActivity extends Activity{
 			}
 		});
 	}
+
+	private void setLocationProviders() {
+		CurrentLocation currentLocation = new CurrentLocation(locationManager);
+		currentLocation.setLocationProvider(locationManager);
+		lastBestKnownLocation = currentLocation.getBestLastKnownLocation();
+	}
 	
 	private void triggerUpload(){
 		Log.i("UploadDetailsActivity","Uploading the photo!");
 			try {
 				String twitpic_url = twitPicUpload.uploadImageFor(imagePath, description.getText().toString());
-				Location location = getCurrentLocation();
 		         // prepare post method  
 		         HttpPost post = new HttpPost(getString(R.string.webservice_url_post));  
 		   
@@ -70,8 +76,8 @@ public class UploadDetailsActivity extends Activity{
 		         List <NameValuePair> parameters = new ArrayList <NameValuePair>();  
 		         parameters.add(new BasicNameValuePair("url", twitpic_url));  
 		         parameters.add(new BasicNameValuePair("description", description.getText().toString()));
-		         parameters.add(new BasicNameValuePair("latitude",  location!=null?String.valueOf(location.getLatitude()):""));
-		         parameters.add(new BasicNameValuePair("longitude", location!=null?String.valueOf(location.getLongitude()):""));
+		         parameters.add(new BasicNameValuePair("latitude",  lastBestKnownLocation!=null?String.valueOf(lastBestKnownLocation.getLatitude()):"37.422006"));
+		         parameters.add(new BasicNameValuePair("longitude", lastBestKnownLocation!=null?String.valueOf(lastBestKnownLocation.getLongitude()):"-122.084095"));
 		   
 		         UrlEncodedFormEntity sendentity = new UrlEncodedFormEntity(parameters, HTTP.UTF_8);  
 		         post.setEntity(sendentity);   
@@ -90,34 +96,4 @@ public class UploadDetailsActivity extends Activity{
 			}
 	}
 
-	private Location getCurrentLocation(){
-		return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-	}
-	
-	private class MyLocationListener implements LocationListener{
-
-		public void onLocationChanged(Location location) {
-			String message = String.format(
-					"New Location \n Latitude: %1$s \n Longitude: %2$s",
-					location.getLatitude(), location.getLongitude()
-					);
-			Log.d("UploadDetailsActivity", message);
-		}
-
-		public void onProviderDisabled(String provider) {
-			Log.d("UploadDetailsActivity", "Provider disabled by the user, GPS is turned off!");
-		}
-
-		public void onProviderEnabled(String provider) {
-			Log.d("UploadDetailsActivity", "Provider enabled by the user, GPS is truned on!");
-			
-		}
-
-		public void onStatusChanged(String provider, int status, Bundle extras) {
-			Log.d("UploadDetailsActivity", "Provider status changed!");
-			
-		}
-    	
-    }	
-	
 }
